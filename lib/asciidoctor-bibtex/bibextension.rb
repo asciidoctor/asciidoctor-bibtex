0afbc32563ba4895808c7039c76198b384b7b6d0
ProgramFan <zyangmath@gmail.com>
2016-09-13 22:51:14 +0800

    Implement custom latex filter for bibtex-ruby and fix #13

--- lib/asciidoctor-bibtex/bibextension.rb
+++ lib/asciidoctor-bibtex/bibextension.rb
@@ -6,11 +6,43 @@ require 'asciidoctor'
 require 'asciidoctor/extensions'
 require 'asciidoctor/reader'
 require 'asciidoctor/parser'
+require 'bibtex/filters'
+require 'latex/decode/base'
+require 'latex/decode/maths'
+require 'latex/decode/accents'
+require 'latex/decode/diacritics'
+require 'latex/decode/punctuation'
+require 'latex/decode/symbols'
+require 'latex/decode/greek'
 require_relative 'styles'
 require_relative 'filehandlers'
 
 module AsciidoctorBibtex
   module Asciidoctor
+
+    # This filter extends the original latex filter in bibtex-ruby to handle
+    # unknown latex macros more gracefully. We could have used latex-decode
+    # gem together with our custom replacement rules, but latex-decode eats up
+    # all braces after it finishes all decoding. So we hack over the
+    # LaTeX.decode function and insert our rules before `strip_braces`.
+    class LatexFilter < ::BibTeX::Filter
+      def apply(value)
+        text = value.to_s
+        LaTeX::Decode::Base.normalize(text)
+        LaTeX::Decode::Maths.decode!(text)
+        LaTeX::Decode::Accents.decode!(text)
+        LaTeX::Decode::Diacritics.decode!(text)
+        LaTeX::Decode::Punctuation.decode!(text)
+        LaTeX::Decode::Symbols.decode!(text)
+        LaTeX::Decode::Greek.decode!(text)
+        text.gsub!(/\\url\{(.+?)\}/, " \\1 ")
+        text.gsub!(/\\\w+(?=\s+\w)/, "")
+        text.gsub!(/\\\w+(?:\[.+?\])?\s*\{(.+?)\}/, "\\1")
+        LaTeX::Decode::Base.strip_braces(text)
+        LaTeX.normalize_C(text)
+      end
+    end
+
     BibliographyBlockMacroPlaceholder = %(BIBLIOGRAPHY BLOCK MACRO PLACEHOLDER)
 
     # This macro processor does only half the work. It just parse the macro
@@ -45,8 +77,8 @@ module AsciidoctorBibtex
       def process document
         bibtex_file = (document.attr 'bibtex-file').to_s
         bibtex_style = ((document.attr 'bibtex-style') || 'ieee').to_s
-        bibtex_order = ((document.attr 'bibtex-order') || 'alphabetical').to_sym
-        bibtex_output = ((document.attr 'bibtex-output') || 'asciidoc').to_sym
+        bibtex_order = ((document.attr 'bibtex-order') || 'appearance').to_sym
+        bibtex_format = ((document.attr 'bibtex-format') || 'asciidoc').to_sym
 
         if bibtex_file.empty?
           bibtex_file = AsciidoctorBibtex::FileHandlers.find_bibliography "."
@@ -59,8 +91,8 @@ module AsciidoctorBibtex
           exit
         end
 
-        bibtex = BibTeX.open bibtex_file, :filter => :latex
-        processor = Processor.new bibtex, true, bibtex_style, bibtex_order == :appearance, bibtex_output
+        bibtex = BibTeX.open bibtex_file, :filter => [LatexFilter]
+        processor = Processor.new bibtex, true, bibtex_style, bibtex_order == :appearance, bibtex_format
 
         prose_blocks = document.find_by {|b| b.content_model == :simple or b.context == :list_item}
         prose_blocks.each do |block|
@@ -91,7 +123,7 @@ module AsciidoctorBibtex
         end
 
         references_asciidoc = []
-        if bibtex_output == :latex
+        if bibtex_format == :latex
           references_asciidoc << %(+++\\bibliography{#{bibtex_file}}{}+++)
           references_asciidoc << %(+++\\bibliographystyle{#{bibtex_style}}+++)
         else
