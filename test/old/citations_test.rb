288399bd6c5d963ac1b4481dcd6647aec54933c4
ProgramFan <zyangmath@gmail.com>
2019-08-05 00:11:55 +0800

    Begin porting tests

--- test/old/citations_test.rb
+++ test/old/citations_test.rb
@@ -1,23 +1,22 @@
 # Test cases for extracting citations from lines of text
 #
 # Copyright (c) Peter Lane, 2012.
-# Copyright (c) Zhang Yang, 2019.
 # Released under Open Works License, 0.9.2
 
-require_relative 'test_helper'
+require 'test_helper'
 
-describe AsciidoctorBibtex do
+describe AsciidocBib do
 
   it "should extract a simple citation from text" do
     cites = Citations.new
-    cites.add_from_line "some text cite:[author12] more text"
+    cites.add_from_line "some text [cite:author12] more text"
     cites.cites_used.size.must_equal 1
     cites.cites_used[0].must_equal "author12"
   end
 
   it "should extract combinations of citations from text" do 
     cites = Citations.new
-    cites.add_from_line "some text cite:[author12,another11] more text"
+    cites.add_from_line "some text [cite:author12;another11] more text"
     cites.cites_used.size.must_equal 2
     cites.cites_used[0].must_equal "author12"
     cites.cites_used[1].must_equal "another11"
@@ -25,7 +24,7 @@ describe AsciidoctorBibtex do
 
   it "should extract separate groups of citations" do
     cites = Citations.new
-    cites.add_from_line "some text cite:[author12,another11] more text cite:[third10]"
+    cites.add_from_line "some text [cite:author12;another11] more text [cite:third10]"
     cites.cites_used.size.must_equal 3
     cites.cites_used[0].must_equal "author12"
     cites.cites_used[1].must_equal "another11"
@@ -34,7 +33,7 @@ describe AsciidoctorBibtex do
 
   it "should extract citations with page numbers" do
     cites = Citations.new
-    citationdata = cites.retrieve_citations "some text citenp:[author12(1-20),another11(15)]"
+    citationdata = cites.retrieve_citations "some text [citenp:author12,1-20;another11,15]"
     citationdata.size.must_equal 1
     citationdata.first.cites.size.must_equal 2
     citationdata.first.cites[0].ref.must_equal "author12"
@@ -46,7 +45,7 @@ describe AsciidoctorBibtex do
 
   it "should extract page numbers as well as refs" do
     cites = Citations.new
-    citationdata = cites.retrieve_citations "citenp:[author12,another11(15-30),third10(14)]"
+    citationdata = cites.retrieve_citations "[citenp:author12;another11,15-30;third10,14]"
     citationdata.size.must_equal 1
     citationdata.first.cites.size.must_equal 3
     citationdata.first.cites[0].ref.must_equal "author12"
@@ -59,13 +58,13 @@ describe AsciidoctorBibtex do
 
   it "should keep each citation once only" do
     cites = Citations.new
-    cites.add_from_line "citenp:[author12,another11,author12]"
+    cites.add_from_line "[citenp:author12;another11;author12]"
     cites.cites_used.size.must_equal 2
   end
 
   it 'should work with dash in citation' do
     cites = Citations.new
-    cites.add_from_line "cite:[some-author]"
+    cites.add_from_line "[cite:some-author]"
     cites.cites_used.size.must_equal 1
     cites.cites_used[0].must_equal 'some-author'
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,5 @@
-if ENV['COVERAGE']
-  require 'simplecov'
-  SimpleCov.start do
-    add_filter 'test'
-    command_name 'Minitest'
-  end
-end
-
-require 'asciidoc-bib'
 require 'minitest/autorun'
+require_relative '../lib/asciidoctor-bibtex'
 
-include AsciidocBib
+include AsciidoctorBibtex
 
