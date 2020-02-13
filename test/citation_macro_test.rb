8d0c7100013e03661a238c451370faac4f7d4510
ProgramFan <zyangmath@gmail.com>
2020-02-13 11:36:17 +0800

    Fix failing tests caused by API renaming

--- test/citation_macro_test.rb
+++ test/citation_macro_test.rb
@@ -11,13 +11,13 @@ include AsciidoctorBibtex
 
 describe AsciidoctorBibtex do
   it 'should extract a simple citation from text' do
-    cites = CitationMacro.extract_citations 'some text cite:[author12] more text'
+    cites = CitationMacro.extract_macros 'some text cite:[author12] more text'
     cites.size.must_equal 1
     cites[0].items[0].key.must_equal 'author12'
   end
 
   it 'should extract combinations of citations from text' do
-    cites = CitationMacro.extract_citations 'some text cite:[author12,another11] more text'
+    cites = CitationMacro.extract_macros 'some text cite:[author12,another11] more text'
     cites.size.must_equal 1
     cites[0].items.size.must_equal 2
     cites[0].items[0].key.must_equal 'author12'
@@ -25,7 +25,7 @@ describe AsciidoctorBibtex do
   end
 
   it 'should extract separate groups of citations' do
-    cites = CitationMacro.extract_citations 'some text cite:[author12,another11] more text cite:[third10]'
+    cites = CitationMacro.extract_macros 'some text cite:[author12,another11] more text cite:[third10]'
     cites.size.must_equal 2
     cites[0].items.size.must_equal 2
     cites[1].items.size.must_equal 1
@@ -35,7 +35,7 @@ describe AsciidoctorBibtex do
   end
 
   it 'should extract citations with page numbers' do
-    cites = CitationMacro.extract_citations 'some text citenp:[author12(1-20),another11(15)]'
+    cites = CitationMacro.extract_macros 'some text citenp:[author12(1-20),another11(15)]'
     cites.size.must_equal 1
     cites.first.items.size.must_equal 2
     cites.first.items[0].key.must_equal 'author12'
@@ -46,7 +46,7 @@ describe AsciidoctorBibtex do
   end
 
   it 'should extract page numbers as well as refs' do
-    cites = CitationMacro.extract_citations 'citenp:[author12,another11(15-30),third10(14)]'
+    cites = CitationMacro.extract_macros 'citenp:[author12,another11(15-30),third10(14)]'
     cites.size.must_equal 1
     cites.first.items.size.must_equal 3
     cites.first.items[0].key.must_equal 'author12'
@@ -58,7 +58,7 @@ describe AsciidoctorBibtex do
   end
 
   it 'should work with dash in citation' do
-    cites = CitationMacro.extract_citations 'cite:[some-author]'
+    cites = CitationMacro.extract_macros 'cite:[some-author]'
     cites.size.must_equal 1
     cites.first.items[0].key.must_equal 'some-author'
   end
