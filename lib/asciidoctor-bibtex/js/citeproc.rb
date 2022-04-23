50318b4b85d508abcdc5a8a652a8450d328fe308
Leon De Andrade <leondeandrade@mailo.com>
2022-04-24 00:20:33 +0200

    Use entry output option

--- lib/asciidoctor-bibtex/js/citeproc.rb
+++ lib/asciidoctor-bibtex/js/citeproc.rb
@@ -7,13 +7,11 @@ module AsciidoctorBibtex
       end
 
       def render(mode, cite_data) 
-        js_cite = `new Cite(#{@js_bibliography}.data.find(cite => cite.id === #{cite_data[:id]}))`
-
         case mode
         when :bibliography
-           return %x{#{js_cite}.format('bibliography')}, ""
+           return %x{#{@js_bibliography}.format('bibliography', { entry: #{cite_data[:id]}})}, ""
         when :citation
-          return %x{#{js_cite}.format('citation')}
+          return %x{#{@js_bibliography}.format('citation', { entry: #{cite_data[:id]}})}
         else
           raise ArgumentError, "cannot render unknown mode: #{mode.inspect}"
         end
