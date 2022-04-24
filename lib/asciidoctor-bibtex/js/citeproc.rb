b792377981a28cba3bdf8fede133e97f8e089a32
Leon De Andrade <leondeandrade@mailo.com>
2022-04-24 11:27:13 +0200

    Add style and locale option

--- lib/asciidoctor-bibtex/js/citeproc.rb
+++ lib/asciidoctor-bibtex/js/citeproc.rb
@@ -2,6 +2,14 @@ module AsciidoctorBibtex
   `const Cite = require('citation-js')`
   module CiteProc
     class Processor
+      attr_reader :style, :format, :locale
+
+      def initialize(options) 
+        @style = options[:style]
+        @format = options[:format]
+        @locale = options[:locale]
+      end 
+
       def import(js_bibliography)
         @js_bibliography = js_bibliography
       end
@@ -9,9 +17,17 @@ module AsciidoctorBibtex
       def render(mode, cite_data) 
         case mode
         when :bibliography
-           return %x{#{@js_bibliography}.format('bibliography', { entry: #{cite_data[:id]}})}, ""
+          return %x{#{@js_bibliography}.format('bibliography', { 
+            entry: #{cite_data[:id]}, 
+            template:  #{@template},
+            locale:  #{@locale}
+          })}, ""
         when :citation
-          return %x{#{@js_bibliography}.format('citation', { entry: #{cite_data[:id]}})}
+          return %x{#{@js_bibliography}.format('citation', { 
+            entry: #{cite_data[:id]},
+            template:  #{@template},
+            locale:  #{@locale}
+          })}
         else
           raise ArgumentError, "cannot render unknown mode: #{mode.inspect}"
         end
