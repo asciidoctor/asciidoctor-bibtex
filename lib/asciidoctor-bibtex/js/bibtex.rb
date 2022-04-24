e07b20a7e3ada3870b9285ca37c8660d4109c81f
Leon De Andrade <leondeandrade@mailo.com>
2022-04-24 11:08:04 +0200

    Remove unneeded files

--- lib/asciidoctor-bibtex/js/bibtex.rb
+++ lib/asciidoctor-bibtex/js/bibtex.rb
@@ -16,7 +16,9 @@ module AsciidoctorBibtex
       end
 
       def [](key)
-        return Entry.new(`#{@js_bibliography}.data.find(cite => cite.id === #{key})`)
+        # return Entry.new(`#{@js_bibliography}.data.find(cite => cite.id === #{key})`)
+        # return Entry.new(%x{#{@js_bibliography}.format('bibtex', { format: 'object',  entry: #{key}})})
+        return Entry.new(%x{#{@js_bibliography}.format('bibtex', { format: 'object'}).find(cite => cite.label === #{key})})
       end
     end
 
@@ -24,14 +26,17 @@ module AsciidoctorBibtex
       attr_reader :author, :editor, :year
 
       def initialize(js_entry)
-        if `js_entry.hasOwnProperty('author')`
-          @author = %x{js_entry.author.map(author => `${author.family}, ${author.given}`).join(' and ')}
-          @editor = @author
-        else
-          @author = nil
-          @editor = %x{js_entry.editor.map(editor => `${editor.family}, ${editor.given}`).join(' and ')}
-        end
-        @year = `js_entry.year`
+        puts "Entry!"
+        `console.log(js_entry)`
+        `console.log(js_entry.author)`
+        # if `js_entry.hasOwnProperty('author')`
+        #   @author = %x{js_entry.author.map(author => `${author.family}, ${author.given}`).join(' and ')}
+        #   @editor = @author
+        # else
+        #   @author = nil
+        #   @editor = %x{js_entry.editor.map(editor => `${editor.family}, ${editor.given}`).join(' and ')}
+        # end
+        # @year = `js_entry.year`
       end
     end
   end
